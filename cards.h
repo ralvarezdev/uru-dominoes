7020d47346c05cb80d093eb624a12045efd1561b
Mister B <86166683+blackb0x2@users.noreply.github.com>
2023-07-31 22:44:10 -0400

    Update cards.h

--- cards.h
+++ cards.h
@@ -11,8 +11,6 @@ using std::vector;
 class Dominoes{
     public:
         int sideA, sideB;
-        bool played = false;
-        int xPos, yPos;
 
         bool convertIntToCard(bool newGame, int numberPlayers, int cardNumber);
 };
@@ -20,25 +18,21 @@ class Dominoes{
 class Board{
     public:
         bool teams = false;
-        int xSize, ySize;
-        int xCenter, yCenter;
-        int cardPlayedUp, cardPlayedDown;
         bool equalTilesUp, equalTilesDown;
-        int colRight, colLeft, rowUp, rowDown;
-        int upSegment, downSegment;
-        int cardsPlayed = 0;
+        int upSegmentC = 0, downSegmentC = 0, numberCardsPlayed = 0;
+        int xSize, ySize, xCenter, yCenter, cardUp, cardDown, colRight, colLeft, upSegment, downSegment, rowUp, rowDown;
+        vector<int> points;
+        vector<string> players;
         vector<vector<bool>> boardCellsOccupied;
         vector<vector<int>> boardCellsContent;
-        vector<string> players;
-        vector<int> points;
 
         void boardSize(int x, int y);
-        void printTitleBar(string title, int numberPlayers);
+        void printTitleBar(string& title, int numberPlayers);
         void printBoard();
-        void addCard(Dominoes domino);
+        void addCard(Dominoes& domino);
         void changeCellContent(bool equalTiles, bool lieDown, int row, int col, int sideFree, int free, int sideConnected = -1, int connected = -1, int separator = -1);
-        void checkUpPosition(bool equalTiles, int *row, int *col, bool *lieDow, bool *separatorn);
-        void checkDownPosition(bool equalTiles, int *row, int *col, bool *lieDown, bool *separator);
+        void checkUpPosition(bool equalTiles, int& row, int& col, bool& lieDown);
+        void checkDownPosition(bool equalTiles, int& row, int& col, bool& lieDown);
 };
 
 class Player{
@@ -48,19 +42,18 @@ class Player{
         int teamNumber = -1;
         string nickname;
         vector<Dominoes> cards;
-        vector<int> cardsPlayed;
+        vector<bool> cardsPlayed;
 
-        int addCards(bool newGame, vector<int> shuffledCards, int players, int cardsPerPlayer, int startRange, int endRange);
+        void addCards(bool startsGame, vector<int>& shuffledCards, int numberPlayers, int cardsPerPlayer);
         bool canPlay(int cardPlayedUp, int cardPlayedDown);
-        void playCard(Board *dominoesBoard, int cardsPerPlayer, int sixSixPosition = -1);
+        void playCard(Board& dominoesBoard, int cardsPerPlayer, int sixSixPosition = -1);
         int countPointsLeft();
         void printCardsLeft(int numberPlayers);
         void playerWinRound(bool teams, int points);
-        bool playerWinGame(bool teams, int pointsToWin);
-        void winBackground(int numberPlayers);
+        void playerWinGame(Board& dominoesBoard);
 };
 
 vector<int> shuffleCards(int numberCards, int players);
-void clearAndTitle(string title);
+void clearAndTitle(string& title);
 
 #endif
